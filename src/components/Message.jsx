c162af59a291f32799da3429d33de87bfdd45d0c
ajshemi <akpojotor@yahoo.com>
2020-06-27 14:45:08 -0400

    updates

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,63 +2,55 @@ import React from "react"; // makes jsx happen
 import PropTypes from "prop-types";
 // import React from "react";
 
-function Message({ name = "Eric", age = 10, sport = "Soccer" }) {
-  return (
-    <div>
-      <>
+// function Message({ name = "Eric", age = 10, sport = "Soccer", children }) {
+//   return (
+//     <div>
+//       <h3>
+//         My name is {name}, I am {age} old and I love playing {sport}{" "}
+//       </h3>
+//       <div>{children}</div>
+//     </div>
+//   );
+// }
+
+class Message extends React.Component {
+  // static defaultProps = {
+  //   name: "Eric",
+  //   age: 10,
+  //   sport: "Soccer",
+  //   // nounOfOurChoice: "Kiwi",
+  // };
+  render() {
+    console.log("what is this", this);
+    let name = this.props.name;
+    let age = this.props.age;
+    let sport = this.props.sport;
+    let children = this.props.children;
+
+    return (
+      <div>
         <h3>
           My name is {name}, I am {age} old and I love playing {sport}{" "}
         </h3>
-        {/* <p>I love {nounOfOurChoice}</p> */}
-        {/* <p>This is {bool ? "true" : "false"}</p> */}
-        {/* <div>{this.props.children}</div> */}
-      </>
-    </div>
-  );
+        <div>{children}</div>
+      </div>
+    );
+  }
 }
-
-export default Message;
-//class component used for state/lifecycle methods
-//class componenet //example teachers/students models should have several folders
-// class Message extends React.Component {
-// static defaultProps = {
-//   name: "Eric",
-//   age: 10,
-//   sport: "Soccer",
-//   // nounOfOurChoice: "Kiwi",
-// };
-// render() {
-// console.log("THIS", this.props); //this.props is an object//console log before return//"this" is a reference to the message instance
-// let name = this.props.name; //dynamically generated
-// let age = this.props.age;
-// let sport = this.props.sport;
-// let nounOfOurChoice = this.props.nounOfOurChoice;
-// let bool = this.props.bool;
-// let what = this.props.what;
-//can only return one object
-// return (
-//   <>
-//     <h3>
-//       My name is {name}, I am {age} old and I love playing {sport}{" "}
-//     </h3>
-//     {/* <p>I love {nounOfOurChoice}</p> */}
-//     <p>This is {bool ? "true" : "false"}</p>
-//     {/* <div>{this.props.children}</div> */}
-//   </>
-// );
-// }
-// }
 
 // Message.defaultProps = {
 //   name: "Eric",
 //   age: 10,
 //   sport: "Soccer",
 // };
+
+export default Message;
 Message.propTypes = {
   // what: PropTypes.element,
   name: PropTypes.string,
   age: PropTypes.number,
-  sport: PropTypes.number,
+  sport: PropTypes.string,
+  // children: PropTypes.element.isRequired,
   // nounOfOurChoice: PropTypes.string,
 };
 //use static defaultProps in a class components
